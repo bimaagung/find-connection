e2c2a58c59bb88a0d92c9cd1749fc84ab1e18beb
bimaagung <bimaagung.labs@gmail.com>
2022-09-06 11:37:35 +0700

    add feature update and delete user

--- test/unit/user.test.js
+++ test/unit/user.test.js
@@ -6,8 +6,10 @@ jest.mock('../../repository/user')
 const userRepository = new UserRepository()
 const userUC = new UserUC(userRepository)
 
-test('test mock user class findByEmail', async () => {
-   const user = {
+describe('test post ./user'),
+  () => {
+    test('test mock user class findByEmail', async () => {
+      const user = {
         id: 1,
         name: 'Bima',
         birth_date: '1996-12-12T17:00:00.000Z',
@@ -17,25 +19,50 @@ test('test mock user class findByEmail', async () => {
         updatedAt: '2022-09-05T04:45:17.347Z',
       }
 
-  userRepository.getUserByEmail.mockReturnValueOnce(user)
+      userRepository.getUserByEmail.mockReturnValueOnce(user)
 
-  expect(await userUC.getUserByEmail('bimaagungsetya@gmail.com')).toEqual(user)
-  expect(userRepository.getUserByEmail).toHaveBeenCalled()
-  expect(userRepository.getUserByEmail).toHaveBeenCalledWith(
-    'bimaagungsetya@gmail.com',
-  )
-})
+      expect(await userUC.getUserByEmail('bimaagungsetya@gmail.com')).toEqual(
+        user,
+      )
+      expect(userRepository.getUserByEmail).toHaveBeenCalled()
+      expect(userRepository.getUserByEmail).toHaveBeenCalledWith(
+        'bimaagungsetya@gmail.com',
+      )
+    })
 
-test('test mock user class createUser', async () => {
-   const user = {
+    test('test mock user return user not found', async () => {
+      const user = {
         name: 'Bima',
         birth_date: '1996-12-12T17:00:00.000Z',
         email: 'bimaagungsetya@gmail.com',
         password: '12345678',
       }
 
-  await userUC.createUser(user)
+      userRepository.getUserByEmail.mockReturnValueOnce(user)
 
-  expect(userRepository.createUser).toHaveBeenCalled()
-  expect(userRepository.createUser).toHaveBeenCalledWith(user)
-})
+      expect(await userUC.getUserByEmail('bimaagungsetya@gmail.com')).toEqual(
+        user,
+      )
+      expect(userRepository.getUserByEmail).toHaveBeenCalled()
+      expect(userRepository.getUserByEmail).toHaveBeenCalledWith(
+        'bimaagungsetya@gmail.com',
+      )
+    })
+  }
+
+describe('test post ./create'),
+  () => {
+    test('test mock user class createUser', async () => {
+      const user = {
+        name: 'Bima',
+        birth_date: '1996-12-12T17:00:00.000Z',
+        email: 'bimaagungsetya@gmail.com',
+        password: '12345678',
+      }
+
+      await userUC.createUser(user)
+
+      expect(userRepository.createUser).toHaveBeenCalled()
+      expect(userRepository.createUser).toHaveBeenCalledWith(user)
+    })
+  }
